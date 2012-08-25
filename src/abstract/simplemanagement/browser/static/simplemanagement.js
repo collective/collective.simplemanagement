ca4358ce8e668eeeb68586adda7915098abfb4eb
Giorgio Borelli <casaborelli@gmail.com>
2012-08-25 14:53:24 +0200

    changed change status tooltip

--- src/abstract/simplemanagement/browser/static/simplemanagement.js
+++ src/abstract/simplemanagement/browser/static/simplemanagement.js
@@ -33,44 +33,36 @@ $(document).ready(function(){
     revert: true
   });
 
-  function get_transition_link(data, url, trigger) {
-    var link = $('<a></a>');
-    var dest_status = data['destination'];
-    var story_container = trigger.parents("li");
-    link.attr('href', url + '/change_status?action=' + data['id']);
-    link.attr('title', data['description']);
-    link.text(data['title']);
-    link.bind('click', function(event){
-      var el = $(this);
-      $.getJSON(el.attr('href'), function(data) {
-        if (data == true) {
-          story_container.removeClass('done in_progress suspended todo');
-          story_container.addClass(dest_status);
-        } else {
-          alert('An error occurred');
-        }
-
-      });
-      event.preventDefault();
-    });
-    return link;
-  }
 
   $('.status').tooltip({
     events: {
       def: "click,blur",
     },
+
     onBeforeShow: function(){
       var tip = this.getTip();
+      tip.empty();
       var trigger = this.getTrigger();
       var url = trigger.attr('href');
-      var content = $('<div></div>');
-      $.getJSON(url + '/wf_actions', function(data){
-        for (var i=0; i<data.length; i++){
-          content.append(get_transition_link(data[i], url, trigger));
-        }
-        tip.html(content);
-      })
+      var story_container = $(trigger).parents("li");
+      // var dest_status = data['destination'];
+      $.get(url + '/wf_actions', function(data) {
+        tip.html(data);
+        // alert(story_container.attr('class'));
+        var links = $(tip).find('a');
+        links.bind('click', function(evt){
+          evt.preventDefault()
+          $.getJSON($(this).attr('href'), function(data) {
+            if (data == false) {
+              alert('An error occurred');
+            } else {
+              story_container.removeClass('done in_progress suspended todo');
+              story_container.addClass(data);
+              tip.hide();
+            }
+          });
+        });
+      });
     }
 
   });
--- src/abstract/simplemanagement/browser/wfactions.py
+++ src/abstract/simplemanagement/browser/wfactions.py
@@ -1,11 +1,14 @@
 import json
 
 from Products.Five.browser import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from Products.CMFCore.utils import getToolByName
 from Products.CMFCore.WorkflowCore import WorkflowException
 
 
 class WFActions(BrowserView):
+
+    wf_actions_template = ViewPageTemplateFile('templates/wf_actions.pt')
 
     @property
     def wf_tool(self):
@@ -14,25 +17,32 @@ class WFActions(BrowserView):
     def wf_actions(self):
         """return a list of transition available for the context
         """
-        actions = []
+        actions = {}
+        ordered_actions = ['start', 'complete', 'suspend', 'reopen']
         wf = self.wf_tool.getWorkflowsFor(self.context)[0]
+
+        for k, v in wf.transitions.items():
+            actions[k] = {
+                'id': v.id,
+                'title': v.title,
+                'description': v.description,
+                'available': False,
+                'destination': v.new_state_id
+            }
+
         for i in wf.listActionInfos(object=self.context):
             if i['available'] and i['allowed']:
-                # TODO: we need this check?
-                tr = i['transition']
-                actions.append({
-                    'destination': tr.new_state_id,
-                    'description': tr.description,
-                    'id': i['id'],
-                    'title': i['title']
-                })
-        return json.dumps(actions)
+                actions[i['id']]['available'] = True
+
+        self.actions = [actions[i] for i in ordered_actions]
+        return self.wf_actions_template()
 
     def change_status(self):
         """Perform a workflow transaction
         and return current workflow status"""
         action = self.request.get('action')
-        success = True
+        success = self.request.get('destination')
+
         try:
             self.wf_tool.doActionFor(self.context, action)
         except WorkflowException:
