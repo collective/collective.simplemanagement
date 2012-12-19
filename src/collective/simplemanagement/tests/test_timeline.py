39224b4b4d4e6136565070fecfb2c10e5a2f70fd
Simone Deponti <simone.deponti@gmail.com>
2012-12-19 21:00:48 +0100

    Now with more tests!

--- src/collective/simplemanagement/tests/test_timeline.py
+++ src/collective/simplemanagement/tests/test_timeline.py
@@ -53,11 +53,60 @@ class TestTimeline(unittest.TestCase):
             ((before, 1), (now, 2))
         )
 
-    def test_snapshot_partial(self):
-        pass
+    @patch('collective.simplemanagement.timeline.datetime', autospec=True)
+    def test_snapshot_partial(self, mock_datetime):
+        one = datetime(2012, 12, 15, 0, 0, 0)
+        mock_datetime.now.return_value = one
+        timeline = DummyTimeline()
+        self.assertNotIn('a', timeline.data)
+        self.assertNotIn('b', timeline.data)
+        timeline.snapshot()
+        self.assertIn('a', timeline.data)
+        self.assertIn('b', timeline.data)
+        self.assertEqual(
+            tuple(timeline.data['a'].items()),
+            ((one, 1),)
+        )
+        self.assertEqual(
+            tuple(timeline.data['b'].items()),
+            ((one, 1),)
+        )
+        two = datetime(2012, 12, 16, 0, 0, 0)
+        mock_datetime.now.return_value = two
+        timeline.snapshot(indexes=['a'])
+        self.assertEqual(
+            tuple(timeline.data['a'].items()),
+            ((one, 1), (two, 2))
+        )
+        self.assertEqual(
+            tuple(timeline.data['b'].items()),
+            ((one, 1),)
+        )
+        three = datetime(2012, 12, 17, 0, 0, 0)
+        mock_datetime.now.return_value = three
+        timeline.snapshot(indexes=['b'])
+        self.assertEqual(
+            tuple(timeline.data['a'].items()),
+            ((one, 1), (two, 2))
+        )
+        self.assertEqual(
+            tuple(timeline.data['b'].items()),
+            ((one, 1), (three, 2))
+        )
 
     def test_snapshot_noinsert(self):
-        pass
+        timeline = DummyTimeline()
+        self.assertNotIn('a', timeline.data)
+        self.assertNotIn('b', timeline.data)
+        result = timeline.snapshot(insert=False)
+        self.assertIn('a', timeline.data)
+        self.assertIn('b', timeline.data)
+        self.assertEqual(len(timeline.data['a'].items()), 0)
+        self.assertEqual(len(timeline.data['b'].items()), 0)
+        self.assertEqual(
+            result,
+            { 'a': 1, 'b': 1 }
+        )
 
     def test_slice(self):
         pass
