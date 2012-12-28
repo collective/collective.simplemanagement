22346eab729419ac9f3ea90b3275d7b81cb2a0b1
Simone Deponti <simone.deponti@gmail.com>
2012-12-28 13:30:56 +0100

    Completed ``IBookingHole`` interfaces.

--- src/collective/simplemanagement/interfaces.py
+++ src/collective/simplemanagement/interfaces.py
@@ -304,7 +304,7 @@ class ISettings(Interface):
 
 
 class IBookingHole(Interface):
-    """Explains why someone seemigly hasn't worked on anything for many hours.
+    """Explains why someone seemingly hasn't worked on anything for many hours.
 
     Since bookings keep track of worked time on projects,
     there can be hours of unbooked time for various reasons.
@@ -320,3 +320,27 @@ class IBookingHole(Interface):
         title=_(u"Reason"),
         source="collective.simplemanagement.off_duty_reasons"
     )
+
+
+class IBookingHoles(Interface):
+    """The list of booking holes.
+
+    See :class:`IBookingHole` for details.
+    """
+
+    def add(hole):
+        """Adds a hole.
+
+        ``hole`` must provide :class:`IBookingHole`
+        """
+
+    def remove(user_id, day):
+        """Removes the hole registered by ``user_id`` for the given ``day``
+        """
+
+    def iter_user(user_id, from_, to):
+        """Return an iterator over the holes inserted for ``user_id``,
+        between ``from_`` and ``to``.
+
+        ``from_`` and ``to`` shall be :class:`~datetime.date` objects.
+        """
