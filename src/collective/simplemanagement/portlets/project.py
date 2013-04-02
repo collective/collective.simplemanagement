c43e27be5861c94f6944a35c030fb2243bfb5b13
Giorgio Borelli <casaborelli@gmail.com>
2013-04-02 16:59:15 +0200

    fix project portlet link

--- src/collective/simplemanagement/portlets/project.py
+++ src/collective/simplemanagement/portlets/project.py
@@ -1,3 +1,4 @@
+from Acquisition import aq_inner
 from plone.portlets.interfaces import IPortletDataProvider
 from plone.app.i18n.locales.browser.selector import LanguageSelector
 from zope.component import getMultiAdapter
@@ -31,24 +32,31 @@ class Renderer(base.Renderer):
 
     @property
     def _project(self):
-        return get_project(self.context)
+        return get_project(aq_inner(self.context))
 
     def _actions(self):
         pa = getToolByName(self.context, 'portal_actions')
-        return pa.simplemanagement_actions.values()
+        return pa.listActionInfos(
+            categories=['simplemanagement_actions']
+        )
 
     def actions(self):
         project_url = self._project.absolute_url()
         expr_context = getExprContext(self.context)
         for i in self._actions():
+            current = self.request['URL'].split('/')[-1]
             yield {
-                'title': i.title,
-                'id': i.id,
+                'title': i['title'],
+                'id': i['id'],
                 'url': '/'.join((
                     project_url,
-                    i.url_expr_object(expr_context)
+                    i['url']
                 )),
-                'description': i.description
+                'description': i['description'],
+                'class': ' '.join((
+                    i['id'],
+                    current == i['id'] and 'selected' or ''
+                ))
             }
 
     render = ViewPageTemplateFile('project.pt')
