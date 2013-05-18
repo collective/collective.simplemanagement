2b8fc43133f0167f7bcb7f426d005a69e7765e05
Simone Deponti <simone.deponti@gmail.com>
2013-05-18 11:55:17 +0200

    Initial work on compass.

---
+++ src/collective/simplemanagement/browser/static/compass.js
@@ -0,0 +1,15 @@
+(function($) {
+    if ((typeof window.simplemanagement) === "undefined") {
+        window.simplemanagement = {};
+    }
+    var sm = window.simplemanagement;
+    if ((typeof sm.compass) === "undefined") {
+        sm.compass = {};
+    }
+    var compass = sm.compass;
+
+    compass.Users = function(data) {
+        this._data = data;
+    };
+
+})(jQuery);
