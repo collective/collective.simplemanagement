48b8e751f9f9eeade8c8c794fb0bc4a3ffdd4f70
Simone Orsi <simahawk@gmail.com>
2013-06-11 23:22:01 +0200

    add option for dropping single user holes

--- src/collective/simplemanagement/browser/holes_helpers.py
+++ src/collective/simplemanagement/browser/holes_helpers.py
@@ -14,7 +14,19 @@ class FlushHoles(BrowserView):
     def __call__(self):
         if not self.request.get('do-it-really'):
             # just a guard for accidental calling
-            return 'you must provide a "do-it-really" in order to flush!'
+            return '\n'.join([
+                'you must provide a "do-it-really" in order to flush!',
+                'you can also pass a "userid" for dropping only single user\'s holes'
+            ])
+        result = 'BOOKING HOLES UTILITY FLUSHED'
         util = component.getUtility(IBookingHoles)
-        util.users = OOBTree()
-        return 'BOOKING HOLES UTILITY FLUSHED'
+        if self.request.get('userid'):
+            userid = self.request.get('userid')
+            if userid in util:
+                util.remove(userid)
+                result += ' for user %s' % userid
+            else:
+                result = 'userid %s not found!' % userid
+        else:
+            util.users = OOBTree()
+        return result
