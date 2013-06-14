fce437c5d379e805dd88b58d33e47b6fa2145c6a
Federico C. Guizzardi <cippinofg@gmail.com>
2013-06-14 11:51:19 +0200

    Added interface for utility

--- src/collective/simplemanagement/interfaces/utilities.py
+++ src/collective/simplemanagement/interfaces/utilities.py
@@ -3,8 +3,11 @@ from zope.interface import Interface
 class IMassiveBookingUploader(Interface):
     """ Utility to upload booking ct """
 
-    def uploade_event_list(self,parent=None,events=[]):
+    def uploade_event_list(self,context=None,events=[]):
         """
         Method to create a list of booking content type
         using events list as source
+        A row in events should be a dictionary with fields
+        title:
         """
+        
