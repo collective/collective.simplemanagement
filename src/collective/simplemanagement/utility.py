5ec4991aabb79c18b4bede5c2f34974d5fa8a4a3
Federico C. Guizzardi <cippinofg@gmail.com>
2013-06-14 11:52:18 +0200

    Added utility to create booking

--- src/collective/simplemanagement/utility.py
+++ src/collective/simplemanagement/utility.py
@@ -1,12 +1,21 @@
 
 from zope.interface import implements
 
+from z3c.form import form, field
+
 from . import _
 from . import logger
+from .booking import create_booking
 from .interfaces import IMassiveBookingUploader
+from .interfaces import IRowMassiveBookingUploader
 
 class MassiveBookingUploader(object):
     implements(IMassiveBookingUploader)
 
-    def uploade_event_list(self,parent=None,events=[]):
-        pass
+    def uploade_event_list(self,context=None,events=[]):
+        fields=field.Fields(IRowMassiveBookingUploader)
+        keys=fields.keys()
+
+        for row in events:            
+            create_booking(context, data, reindex=False)
+        
