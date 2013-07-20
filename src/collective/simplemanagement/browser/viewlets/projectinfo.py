0edb376ff592bdcc9f4219e828f14923f96d1e23
Giorgio Borelli <casaborelli@gmail.com>
2013-07-20 21:03:26 +0200

    fix get_project and add documentation and tracker button in projectinfo viewlet

--- src/collective/simplemanagement/browser/viewlets/projectinfo.py
+++ src/collective/simplemanagement/browser/viewlets/projectinfo.py
@@ -2,7 +2,9 @@ from urllib import unquote
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone.app.layout.viewlets import common as base
 
+from collective.simplemanagement.configure import DOCUMENTS_ID, TRACKER_ID
 from collective.simplemanagement.utils import get_project
+from collective.simplemanagement import _
 
 
 class ProjectInfo(base.ViewletBase):
@@ -29,3 +31,49 @@ class ProjectInfo(base.ViewletBase):
             'project_url': self.project.absolute_url(),
             'project_title': self.project.Title()
         }
+
+    def buttons(self):
+        info = self.info
+        buttons = [
+            {
+                'url': "%s/@@dashboard" % info['project_url'],
+                'title': _(u'Dashboard')
+            },
+            {
+                'url': "%s/@@alliterations" % info['project_url'],
+                'title': _(u'Iterations')
+            },
+            {
+                'url': "%s/@@backlog" % info['project_url'],
+                'title': _(u'Backlog')
+            },
+            {
+                'url': "%s/@@worklog" % info['project_url'],
+                'title': _(u'Worklog')
+            },
+            {
+                'url': "%s/@@planning" % info['project_url'],
+                'title': _(u'Planning')
+            },
+            {
+                'url': "%s/@@report" % info['project_url'],
+                'title': _(u'Report')
+            },
+            {
+                'url': '/'.join((info['project_url'], DOCUMENTS_ID)),
+                'title': _(u"Documentation")
+
+            },
+            {
+                'url': '/'.join((info['project_url'], TRACKER_ID)),
+                'title': _(u"Tracker")
+            }
+        ]
+
+        if info['is_project']:
+            buttons.insert(0, {
+                'url': info['project_url'],
+                'title': _(u'Go to Project')
+            })
+
+        return buttons
