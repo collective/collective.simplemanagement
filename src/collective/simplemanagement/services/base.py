536623d557171f4b3072a7aeb08f14e5389acb0a
Giorgio Borelli <casaborelli@gmail.com>
2013-11-08 13:09:16 +0100

    add docstring to services module

--- src/collective/simplemanagement/services/base.py
+++ src/collective/simplemanagement/services/base.py
@@ -14,7 +14,14 @@ class JSONService(BrowserView):
     action = ''
 
     def publishTraverse(self, request, name):
-        """Set view action"""
+        """Set the action of this view:
+        * view
+        * modify
+        * list
+        * add
+        * delete
+        * update
+        """
         request['TraversalRequestNameStack'] = []
         if name not in SERVICES:
             raise NotFound(self, "Not found", self.request)
@@ -23,29 +30,43 @@ class JSONService(BrowserView):
 
     @api.permissions.accesscontrol("View")
     def view(self):
-        raise NotImplementedError
+        """Return some properties from the context"""
+        return {
+            'title': self.context.title,
+            'description': self.context.description
+        }
 
     @api.permissions.accesscontrol("Modify Portal Content")
     def modify(self):
+        """Change some properties of the context
+        """
         raise NotImplementedError
 
     @api.permissions.accesscontrol("View")
     def list(self):
+        """Return a list of objects, depends on the context"""
         raise NotImplementedError
 
     @api.permissions.accesscontrol("Manage Portal")
     def add(self):
+        """Add one o more elements to the context"""
         raise NotImplementedError
 
     @api.permissions.accesscontrol("Delete")
     def delete(self):
+        """Delete some objects from the context"""
         raise NotImplementedError
 
     @api.permissions.accesscontrol("Add portal content")
     def update(self):
+        """Update some object on the context"""
         raise NotImplementedError
 
+    @api.jsonutils.jsonmethod()
     def __call__(self):
+        """Return a json based on self.action parameter
+        action represent a method described in this view
+        """
         action = getattr(self, self.action, None)
         if not action:
             return ''
