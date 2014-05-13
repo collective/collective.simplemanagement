ec374a9dff1b825131c37ec4f752347e3cbab330
Giorgio Borelli <casaborelli@gmail.com>
2014-05-13 10:12:11 +0200

    fix searchable text indexers

--- src/collective/simplemanagement/indexers.py
+++ src/collective/simplemanagement/indexers.py
@@ -27,16 +27,17 @@ def date(obj):
 
 
 def SearchableText(obj, text=False):
-    text = [obj.id, obj.title]
-    if obj.description:
-        text.append(obj.description)
-    return ' '.join(text)
+    text = [obj.getId(), obj.Title()]
+    description = obj.Description()
+    if description:
+        text.append(description)
+    return text
 
 
 @indexer(IProject)
 def SearchableText_project(obj):
-    searchable = [SearchableText(obj)]
-    if obj.notes is not None or obj.text.output is None:
+    searchable = SearchableText(obj)
+    if obj.notes is not None or obj.notes.output is None:
         searchable.append(obj.notes.output)
 
     if obj.customer is not None:
@@ -50,21 +51,25 @@ def SearchableText_project(obj):
 
 @indexer(IIteration)
 def SearchableText_iteration(obj):
-    return SearchableText(obj)
+    searchable = SearchableText(obj)
+    return ' '.join(searchable)
 
 
 @indexer(IStory)
 def SearchableText_story(obj):
-    if obj.text is None:
-        return SearchableText(obj)
-    return ' '.join((SearchableText(obj), obj.text))
+    searchable = SearchableText(obj)
+    if obj.text is not None:
+        searchable.append(obj.text)
+    return ' '.join(searchable)
 
 
 @indexer(IEpic)
 def SearchableText_epic(obj):
-    if obj.text is None or obj.text.output is None:
-        return SearchableText(obj)
-    return ' '.join((SearchableText(obj), obj.text.output))
+    searchable = SearchableText(obj)
+    text = obj.get_text()
+    if text is not None:
+        searchable.append(text)
+    return ' '.join(searchable)
 
 
 @indexer(IContentish)
