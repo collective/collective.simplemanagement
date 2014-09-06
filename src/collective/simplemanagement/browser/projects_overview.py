6a579d835b42e2b48b43cfe45bc314a7b7aa1320
Simone Orsi <simahawk@gmail.com>
2014-09-06 20:35:21 +0200

    projects_overview: add filtering by state, styles and fix results enumeration

--- src/collective/simplemanagement/browser/projects_overview.py
+++ src/collective/simplemanagement/browser/projects_overview.py
@@ -1,6 +1,8 @@
 #-*- coding: utf-8 -*-
 
 from plone.app.search.browser import Search
+from plone.memoize import view
+from plone import api
 
 
 class ProjectsOverview(Search):
@@ -10,3 +12,17 @@ class ProjectsOverview(Search):
     allow_portal_types_filter = False
     # this allows for results with no query
     default_query = {'portal_type': 'Project'}
+
+    _states_blacklist = ()
+
+    @view.memoize_contextless
+    def review_states(self):
+        states_blacklist = self._states_blacklist
+        wftool = api.portal.get_tool('portal_workflow')
+        wf_id = wftool.getChainForPortalType(
+            self.default_query['portal_type'])[0]
+        wf = wftool[wf_id]
+        return [{'name': x.id, 'title': x.title}
+                for x in wf.states.objectValues()
+                if x.id not in states_blacklist]
+
