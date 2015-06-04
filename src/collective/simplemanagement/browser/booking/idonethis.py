ca3afcfd3ef64f377bac336f1d260d8e13406923
Simone Orsi <simahawk@gmail.com>
2015-06-04 14:42:37 +0200

    [imp] idonethis perfomance and cron facilities

--- src/collective/simplemanagement/browser/booking/idonethis.py
+++ src/collective/simplemanagement/browser/booking/idonethis.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
 
+import logging
+logger = logging.getLogger('[sm.idonethis]')
+
 from datetime import datetime
 from datetime import timedelta
 
 from zope.component import getMultiAdapter
 # from zope.publisher.interfaces import NotFound
 import plone.api
-# from plone.memoize import view
+from plone.memoize import view
 
 from Products.Five.browser import BrowserView
 # from Products.CMFPlone.utils import safe_unicode
@@ -15,6 +18,7 @@ from Products.statusmessages.interfaces import IStatusMessage
 from ... import _
 from ... import api
 from ... import mail_utils
+from ...utils import LazyList
 
 
 class View(BrowserView):
@@ -25,19 +29,13 @@ class View(BrowserView):
     show_form = True
     default_send_to = 'team@abstract.it'
 
-    def __call__(self, send_email=False):
-        if self.request.get('send_email') \
-                and not send_email \
-                and not self.request.get('search'):
-            self.send_email()
-        return super(View, self).__call__()
-
     @property
     def today(self):
         return datetime.now()
 
     @property
     def current_date(self):
+        # default to the day before today
         dt = self.today - timedelta(1)
         if self.request.get('by_date'):
             dt = self.request.get('by_date')
@@ -67,50 +65,103 @@ class View(BrowserView):
         }
         return query
 
+    def formatter(self, booking):
+        helpers = getMultiAdapter(
+            (booking, self.request),
+            name="helpers"
+        )
+        info = helpers.info(user_details=False,
+                            drop_refs_links=True,
+                            minimal=True)
+        return info
+
+    @view.memoize
+    def get_bookings(self):
+        return api.booking.get_bookings(**self.query)
+
     def data(self):
-        bookings = {}
         users = {}
-        all_bookings = api.booking.get_bookings(**self.query)
-        for booking in all_bookings:
+        bookings = {}
+        for booking in self.get_bookings():
             if not booking.story and not booking.project:
                 continue
-            helpers = getMultiAdapter(
-                (booking, self.request),
-                name="helpers"
-            )
+
             if booking.owner not in users:
                 user_info = api.users.get_user_details(self.context,
                                                        booking.owner)
                 users[booking.owner] = user_info
-                bookings[booking.owner] = []
-
-            info = helpers.info(user_details=False,
-                                drop_refs_links=True,
-                                minimal=True)
-            bookings[booking.owner].append(info)
+                bookings[booking.owner] = LazyList(
+                    [], format_method=self.formatter)
+            bookings[booking.owner].append(booking)
         return {
+            'users': sorted(users.itervalues(), key=lambda x: x.fullname),
             'bookings': bookings,
-            'users': users,
         }
 
+
+class SendEmail(View):
+
+    redirect = True
+
+    def __call__(self):
+        if self.request.get('send_email'):
+            return self.send_email()
+        return "confirm send by providing `send_email` key"
+
     def send_email(self):
-        mailhost = plone.api.portal.get_tool('MailHost')
-        pprops = plone.api.portal.get_tool('portal_properties')
-        mto = self.request.get('send_to', self.default_send_to)
-        mfrom = pprops.email_from_address
-        msubject = _(u"Abstract Team Done This") \
-            + ' ' + self.current_date_display
         view = self.context.restrictedTraverse('@@idonethis-content')
-        view.show_form = False
-        html = view(send_email=True)
-        html = mail_utils.prepare_email_content(
-            html, mfrom, mto, msubject)
-        mailhost.send(html, mto, mfrom, msubject)
-        msg = _('Email sent.')
-        messages = IStatusMessage(self.request)
-        messages.add(msg, type="info")
-        url = self.context.absolute_url() + '/@@idonethis'
-        if self.request.get('QUERY_STRING'):
-            qstring = self.request['QUERY_STRING'].split('&send_email')[0]
-            url += '?' + qstring
-        self.request.response.redirect(url)
+        bookings = view.get_bookings()
+        status = 'ok'
+        if not len(bookings) > 0:
+            status = 'nobookings'
+            logger.info('no bookings found...')
+        else:
+            view.show_form = False
+            html = view()
+
+            mailhost = plone.api.portal.get_tool('MailHost')
+            pprops = plone.api.portal.get_tool('portal_properties')
+            mto = self.request.get('send_to', self.default_send_to)
+            mfrom = pprops.email_from_address
+            msubject = _(u"Abstract Team Done This") \
+                + ' ' + self.current_date_display
+            html = mail_utils.prepare_email_content(
+                html, mfrom, mto, msubject)
+
+            mailhost.send(html, mto, mfrom, msubject)
+            logger.info('mail sent')
+        if self.redirect and self.request.get('status_message'):
+            if status == 'ok':
+                msg = _('Email sent.')
+            else:
+                msg = _('No bookings, no email send.')
+            messages = IStatusMessage(self.request)
+            messages.add(msg, type="info")
+            url = self.context.absolute_url() + '/@@idonethis'
+            if self.request.get('QUERY_STRING'):
+                qstring = self.request['QUERY_STRING'].split('&send_email')[0]
+                url += '?' + qstring
+            self.request.response.redirect(url)
+            return
+        else:
+            return status
+
+
+class CronView(SendEmail):
+
+    redirect = False
+    DATE_FORMAT = '%d/%m/%Y'
+
+    def __call__(self):
+        today = datetime.today()
+        weekday = today.weekday()
+        # TODO: exclude holidays!
+        if weekday in (5, 6):
+            # sat or sun
+            return
+        if weekday == 0:
+            # monday, we want friday
+            dt = today - timedelta(3)
+            by_date = dt.strftime(self.DATE_FORMAT)
+            self.request.form['by_date'] = by_date
+        return self.send_email()
--- src/collective/simplemanagement/utils.py
+++ src/collective/simplemanagement/utils.py
@@ -28,3 +28,33 @@ class AttrDict(dict):
 
     def __setattr__(self, k, v):
         self[k] = v
+
+
+class LazyList(list):
+    """Lazy list with custom formatting for items.
+    Initialize it w/ a set of items and a method
+    that will be used to format returned items.
+    """
+
+    def __init__(self, items, format_method):
+        super(LazyList, self).__init__(items)
+        self.format_method = format_method
+
+    def __getslice__(self, start, stop):
+        """ deprecated but used in python2.7
+        """
+        elements = super(LazyList, self).__getslice__(start, stop)
+        for i in elements:
+            yield self.format_item(i)
+
+    def __getitem__(self, index):
+        elements = super(LazyList, self).__getitem__(index)
+        return self.format_item(elements)
+
+    def __iter__(self):
+        elements = super(LazyList, self).__iter__()
+        for i in elements:
+            yield self.format_item(i)
+
+    def format_item(self, item):
+        return self.format_method(item)
